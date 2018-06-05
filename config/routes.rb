d4f3c781f8781c7423add5e73eeda3c662458833
EC2 Default User <ec2-user@ip-172-31-43-83.us-east-2.compute.internal>
2018-06-05 15:01:59 +0000

    Display a user

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  root  'static_pages#home'
   
+  resources :users, only: [:show]
+  
+   root  'static_pages#home'
+ 
   match '/help',    to: 'static_pages#help',    via: 'get'
   
   match '/about',   to: 'static_pages#about',   via: 'get'
